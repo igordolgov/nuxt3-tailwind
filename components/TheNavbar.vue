8a03321b4bf69f7eb7ff8f5e84205a4258636be8
Igor Dolgov <65899955+igordolgov@users.noreply.github.com>
2023-03-30 03:14:56 +0300

    add images from local store

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -5,8 +5,11 @@ header.flex.items-center.mb-3.h-12.text-white.text-xl.sticky.inset-x-0.top-0.sha
       a(href="#!").h-6.w-6.mr-2.pl-6.bg-white.
         Logo
       .flex
-        Nuxt-link(to="/", class="hover:ripple-bg-red-800 hover:bg-red-700 focus:bg-sky-700").px-6.py-2.transition-color.duration-300.ease-in-out Каталог
-        Nuxt-link(to="NewsPage", class="hover:ripple-bg-red-800 hover:bg-red-700 focus:bg-sky-700").px-6.py-2.transition-color.duration-300.ease-in-out Новости
-        Nuxt-link(to="#!", class="hover:ripple-bg-red-800 hover:bg-red-700 focus:bg-sky-700").px-6.py-2.transition-color.duration-300.ease-in-out Контакты
-        ButtonComponent.ml-4(title="Назад")
+        Nuxt-link(to="/", class="hover:bg-sky-600 focus:border-b-4 focus:border-white").px-6.py-2.border-b-4.border-transparent
+          | Каталог
+        Nuxt-link(to="NewsPage", class="hover:bg-sky-600 focus:border-b-4 focus:border-white").px-6.py-2.border-b-4.border-transparent
+          | Новости
+        Nuxt-link(to="#!", class="hover:bg-sky-600 focus:border-b-4 focus:border-white").px-6.py-2.border-b-4.border-transparent
+          | Контакты
+        ButtonComponent.ml-4(title="Кнопка")
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
 main.container.mx-auto.px-4.min-h-screen
     h1.text-3xl.font-bold.card Моторы
-    img.w-80.h-80(src='../assets/engine1-01.jpg')
-    .grid.grid-cols-1.gap-4.mb-8(class="sm:grid-cols-2 lg:grid-cols-3")
+    .grid.grid-cols-1.gap-4.mb-8(class="sm:grid-cols-2 lg:grid-cols-4")
         .p-6.rounded-lg.shadow-xl.shadow-zinc-300(
             v-for="item in store.motors",
             :key="item.id"
-        ) {{ item.title }}
-          img.w-40.h-40(src='item.images[0]' alt="item.title")
+        )
+          img(:src="item.images[0].src" :alt="item.title")
+          p {{ item.condition }} руб.
           p {{ item.price }} руб.
 </template>
 
 <script setup>
-    // импортируем (подключаем) хранилище
-    import { useCardTexts } from '~~/stores/myStore'
-    // сохраняем в переменную данные из хранилища
-    const store = useCardTexts()
+// импортируем (подключаем) хранилище
+import { useCardTexts } from '~~/stores/products'
+// сохраняем в переменную данные из хранилища
+const store = useCardTexts()
 </script>
